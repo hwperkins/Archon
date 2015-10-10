6dacda0bae5e340efae2b2a1618a7ad48c6017c9
Howard Gehring <hwgehring@gmail.com>
2015-10-10 15:43:18 -0400

    Reordering functions

--- src/DataFrame.php
+++ src/DataFrame.php
@@ -7,67 +7,47 @@ use InvalidArgumentException;
 
 use Assert\Assertion;
 
-class DataFrame implements ArrayAccess, Iterator
-{
+class DataFrame implements ArrayAccess, Iterator {
 
     private $data = [];
     private $pointer = 0;
 
-    private function __construct(array $data)
-    {
+    private function __construct(array $data) {
         $this->data = $data;
     }
 
-    public static function fromArray(array $data)
-    {
-        return new DataFrame($data);
-    }
-
-    public function toArray()
-    {
-        return $this->data;
-    }
-
-    public function offsetGet($key)
-    {
+    public function offsetGet($key) {
         $col = array_column($this->data, $key);
         foreach($col as $i => $row) $col[$i] = [$key => $row];
         return new DataFrame($columns=[$key], $data=$col);
     }
 
-    public function offsetSet($key, $value)
-    {
+    public function offsetSet($key, $value) {
         $this->columns[] = $key;
         foreach($this as $row) $row[$key] = $value;
     }
 
-    public function current()
-    {
+    public function current() {
         return $this->data[$this->key()];
     }
 
-    public function next()
-    {
+    public function next() {
         $this->pointer++;
     }
 
-    public function key()
-    {
+    public function key() {
         return $this->pointer;
     }
 
-    public function valid()
-    {
+    public function valid() {
         return isset($this->data[$this->key()]);
     }
 
-    public function rewind()
-    {
+    public function rewind() {
         $this->pointer = 0;
     }
 
-    public function offsetExists($offset)
-    {
+    public function offsetExists($offset) {
         foreach($this as $row) {
             if (!array_key_exists($offset, $row)) {
                 return FALSE;
@@ -77,8 +57,7 @@ class DataFrame implements ArrayAccess, Iterator
         return TRUE;
     }
 
-    public function offsetUnset($offset)
-    {
+    public function offsetUnset($offset) {
         if (!isset($this[$offset])) {
             throw new \RuntimeException("Key {$offset} not found in DataFrame.");
         }
@@ -90,5 +69,13 @@ class DataFrame implements ArrayAccess, Iterator
         if(($key = array_search($offset, $this->columns)) !== FALSE) {
             unset($this->columns[$key]);
         }
+    }
+
+    public static function fromArray(array $data) {
+        return new DataFrame($data);
+    }
+
+    public function toArray() {
+        return $this->data;
     }
 }
