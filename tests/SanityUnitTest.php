107cbf5214c42e4f8cea34b318df7204b55b80e8
Howard Gehring <hwgehring@gmail.com>
2015-10-10 14:58:37 -0400

    Added build folder Renamed test folder to tests

---
+++ tests/SanityUnitTest.php
@@ -0,0 +1,15 @@
+<?php namespace Archon\Test;
+
+/**
+ * Created by PhpStorm.
+ * User: hwgehring
+ * Date: 8/31/2015
+ * Time: 10:04 AM
+ */
+class SanityUnitTest extends \PHPUnit_Framework_TestCase {
+
+    public function test_sanity() {
+        $this->assertTrue(true);
+    }
+
+}
