5d4d20c793b30f6016c9a0891aea4d58bdc87c9e
Howard Gehring <hwgehring@gmail.com>
2015-10-11 17:50:22 -0400

    Code style fixes

--- src/IO/HTML.php
+++ src/IO/HTML.php
@@ -1,20 +1,25 @@
 <?php namespace Archon\IO;
 
+use Archon\Exceptions\NotYetImplementedException;
+
 /**
  * @link https://github.com/HWGehring/Archon for the canonical source repository
  * @license https://github.com/HWGehring/Archon/blob/master/LICENSE BSD 3-Clause
  */
-final class HTML {
+final class HTML
+{
 
     private $defaultOptions = [
         'readable' => false
     ];
 
-    public function __construct(array $data) {
+    public function __construct(array $data)
+    {
         $this->data = $data;
     }
 
-    public function render(array $options) {
+    public function render(array $options)
+    {
         $data = $this->data;
         $options = Options::setDefaultOptions($options, $this->defaultOptions);
         $readableOpt = $options['readable'];
@@ -26,7 +31,7 @@ final class HTML {
         $header = $this->wrapTHead($columns);
         $footer = $this->wrapTFoot($columns);
 
-        foreach($data as &$row) {
+        foreach ($data as &$row) {
             $row = $this->wrapTRTH($row);
         }
 
@@ -34,27 +39,36 @@ final class HTML {
         $data = $this->wrapTBody($data);
         $data = $header.$footer.$data;
         $data = $this->wrapTable($data);
-        return $data;
+
+        if ($readableOpt === false) {
+            return $data;
+        }
+
+        throw new NotYetImplementedException('Pretty HTML not yet implemented');
     }
 
-    private function wrapTRTH(array $data) {
+    private function wrapTRTH(array $data)
+    {
         return '<tr><th>'.implode('</th><th>', $data).'</th></tr>';
     }
 
-    private function wrapTHead($data) {
+    private function wrapTHead($data)
+    {
         return '<thead>'.$data.'</thead>';
     }
 
-    private function wrapTFoot($data) {
+    private function wrapTFoot($data)
+    {
         return '<tfoot>'.$data.'</tfoot>';
     }
 
-    private function wrapTBody($data) {
+    private function wrapTBody($data)
+    {
         return '<tbody>'.$data.'</tbody>';
     }
 
-    private function wrapTable($data) {
+    private function wrapTable($data)
+    {
         return '<table>'.$data.'</table>';
     }
-
 }
