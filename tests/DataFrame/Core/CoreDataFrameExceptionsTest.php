8cc50b64c0254ae43012905965f715a7f523e726
Howard Gehring <hwgehring@gmail.com>
2015-10-11 18:33:38 -0400

    Code style issues fixed

--- tests/DataFrame/Core/CoreDataFrameExceptionsTest.php
+++ tests/DataFrame/Core/CoreDataFrameExceptionsTest.php
@@ -2,9 +2,11 @@
 
 use Archon\DataFrame;
 
-class CoreDataFrameExceptionsTest extends \PHPUnit_Framework_TestCase {
+class CoreDataFrameExceptionsTest extends \PHPUnit_Framework_TestCase
+{
 
-    public function setUp() {
+    public function setUp()
+    {
         $this->input = [
             ['a' => 1, 'b' => 2, 'c' => 3],
             ['a' => 4, 'b' => 5, 'c' => 6],
@@ -14,30 +16,32 @@ class CoreDataFrameExceptionsTest extends \PHPUnit_Framework_TestCase {
         $this->df = DataFrame::fromArray($this->input);
     }
 
-    public function testInvalidColumn() {
+    public function testInvalidColumn()
+    {
         $this->setExpectedException('Archon\Exceptions\DataFrameException');
         $this->df['foo'];
     }
 
-    public function testRemoveNonExistentColumn() {
+    public function testRemoveNonExistentColumn()
+    {
         $this->setExpectedException('Archon\Exceptions\DataFrameException');
         $this->df->removeColumn('foo');
     }
 
-    public function testInvalidOffsetSet1() {
+    public function testInvalidOffsetSet1()
+    {
         $df = $this->df;
 
         $this->setExpectedException('Archon\Exceptions\DataFrameException');
         $df['foo'] = $df;
     }
 
-    public function testInvalidOffsetSet2() {
+    public function testInvalidOffsetSet2()
+    {
         $df = $this->df;
         $df2 = DataFrame::fromArray([['a' => 1, 'b' => 2, 'c' => 3]]);
 
         $this->setExpectedException('Archon\Exceptions\DataFrameException');
         $df['a'] = $df2['a'];
     }
-
-
 }
