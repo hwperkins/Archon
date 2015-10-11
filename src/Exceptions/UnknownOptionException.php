35d6620dca6fa1f1fe6cdb0d8a0fe92d49f8f405
Howard Gehring <hwgehring@gmail.com>
2015-10-11 17:42:25 -0400

    Added exception for unknown options

---
+++ src/Exceptions/UnknownOptionException.php
@@ -0,0 +1,9 @@
+<?php namespace Archon\Exceptions;
+
+/**
+ * @link https://github.com/HWGehring/Archon for the canonical source repository
+ * @license https://github.com/HWGehring/Archon/blob/master/LICENSE BSD 3-Clause
+ */
+class UnknownOptionException extends DataFrameException {
+
+}
