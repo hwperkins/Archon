98f2580c1b818611738c7f2f605a27e90289ddfc
Howard Gehring <hwgehring@gmail.com>
2015-10-11 18:40:42 -0400

    Code style issues fixed

--- tests/SanityUnitTest.php
+++ tests/SanityUnitTest.php
@@ -1,15 +1,10 @@
 <?php namespace Archon\Test;
 
-/**
- * Created by PhpStorm.
- * User: hwgehring
- * Date: 8/31/2015
- * Time: 10:04 AM
- */
-class SanityUnitTest extends \PHPUnit_Framework_TestCase {
+class SanityUnitTest extends \PHPUnit_Framework_TestCase
+{
 
-    public function test_sanity() {
+    public function testSanity()
+    {
         $this->assertTrue(true);
     }
-
 }
