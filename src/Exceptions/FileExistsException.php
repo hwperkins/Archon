f7e77bdfb95f17963e58b1131735d6a48789fee7
Howard Gehring <hwgehring@gmail.com>
2015-10-11 20:17:19 -0400

    Added meaningful exception.

---
+++ src/Exceptions/FileExistsException.php
@@ -0,0 +1,26 @@
+<?php
+
+/**
+ * Contains the FileExistsException.
+ * @package   DataFrame
+ * @author    Howard Gehring <hwgehring@gmail.com>
+ * @copyright 2015 Howard Gehring <hwgehring@gmail.com>
+ * @license   https://github.com/HWGehring/Archon/blob/master/LICENSE BSD-3-Clause
+ * @link      https://github.com/HWGehring/Archon
+ */
+
+namespace Archon\Exceptions;
+
+/**
+ * Exception thrown when an file type implementation has been instructed to incorrectly write to a file which already
+ * exists.
+ * @package   Archon\Exceptions
+ * @author    Howard Gehring <hwgehring@gmail.com>
+ * @copyright 2015 Howard Gehring <hwgehring@gmail.com>
+ * @license   https://github.com/HWGehring/Archon/blob/master/LICENSE BSD-3-Clause
+ * @link      https://github.com/HWGehring/Archon
+ */
+class FileExistsException extends DataFrameException
+{
+
+}
