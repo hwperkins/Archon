767aa3d05b02b81a7386aa1c4ec0b5c9b37d54ab
Howard Gehring <hwgehring@gmail.com>
2015-10-12 22:46:34 -0400

    Created test and test file for to/from XLSX feature

---
+++ tests/DataFrame/XLSX/XLSXDataFrameUnitTest.php
@@ -0,0 +1,128 @@
+<?php namespace Archon\Tests\DataFrame\XLSX;
+
+use Archon\DataFrame;
+use PHPExcel;
+use PHPExcel_Writer_Excel2007;
+
+class XLSXDataFrameUnitTest extends \PHPUnit_Framework_TestCase
+{
+
+    public function testLoadXLSX()
+    {
+        $fileName = __DIR__.DIRECTORY_SEPARATOR.'TestFiles'.DIRECTORY_SEPARATOR.'test.xlsx';
+
+        $xlsx = DataFrame::fromXLSX($fileName);
+        $xlsx = $xlsx->toArray();
+
+        $assertion_array = [
+            [
+                "col_a" => "physician",
+                "col_b" => "heal",
+                "col_c" => "thyself",
+                "col_d" => "",
+                "col_e" => "lorem",
+                "col_f" => "ipsum",
+            ],
+            [
+                "col_a" => "",
+                "col_b" => "physician",
+                "col_c" => "heal",
+                "col_d" => "thyself",
+                "col_e" => "",
+                "col_f" => "lorem",
+            ],
+            [
+                "col_a" => "ipsum",
+                "col_b" => "",
+                "col_c" => "physician",
+                "col_d" => "heal",
+                "col_e" => "thyself",
+                "col_f" => ""
+            ],
+            [
+                "col_a" => "lorem",
+                "col_b" => "ipsum",
+                "col_c" => "",
+                "col_d" => "physician",
+                "col_e" => "heal",
+                "col_f" => "thyself",
+            ]
+        ];
+
+        $this->assertEquals($assertion_array, $xlsx);
+    }
+
+    public function testToXLSX()
+    {
+        $fileName = __DIR__.DIRECTORY_SEPARATOR.'TestFiles'.DIRECTORY_SEPARATOR.'test_to.xlsx';
+
+        if (file_exists($fileName)) {
+            unlink($fileName);
+        }
+
+        $sheetA = [
+            [
+                'a' => 'one',
+                'b' => 'two',
+                'c' => 'three'
+            ],
+            [
+                'a' => 'four',
+                'b' => 'five',
+                'c' => 'six'
+            ]
+        ];
+
+        $sheetB = [
+            [
+                'd' => 'seven',
+                'e' => 'eight',
+                'f' => 'nine'
+            ],
+            [
+                'd' => 'ten',
+                'e' => 'eleven',
+                'f' => 'twelve'
+            ]
+        ];
+
+        $sheetC = [
+            [
+                'g' => 'thirteen',
+                'h' => 'fourteen',
+                'i' => 'fifteen'
+            ],
+            [
+                'g' => 'sixteen',
+                'h' => 'seventeen',
+                'i' => 'eighteen'
+            ]
+        ];
+
+        $a = DataFrame::fromArray($sheetA);
+        $b = DataFrame::fromArray($sheetB);
+        $c = DataFrame::fromArray($sheetC);
+
+        $xlsx = new PHPExcel();
+        $a->toWorksheet($xlsx, 'A');
+        $b->toWorksheet($xlsx, 'B');
+        $c->toWorksheet($xlsx, 'C');
+
+        $this->assertEquals($a->toArray(), $sheetA);
+        $this->assertEquals($b->toArray(), $sheetB);
+        $this->assertEquals($c->toArray(), $sheetC);
+
+        $writer = new PHPExcel_Writer_Excel2007($xlsx);
+        $writer->save($fileName);
+
+        $a = DataFrame::fromXLSX($fileName, ['sheet_name' => 'A']);
+        $b = DataFrame::fromXLSX($fileName, ['sheet_name' => 'B']);
+        $c = DataFrame::fromXLSX($fileName, ['sheet_name' => 'C']);
+
+        $this->assertEquals($a->toArray(), $sheetA);
+        $this->assertEquals($b->toArray(), $sheetB);
+        $this->assertEquals($c->toArray(), $sheetC);
+
+        unlink($fileName);
+    }
+}
