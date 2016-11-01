6e2d6186f6b38c21a792040ecfda08c2a432e1bb
Howard Gehring <howard.gehring@mshallassociates.com>
2016-11-01 10:41:45 -0400

    $input is no longer dynamically declared

--- tests/DataFrame/Core/CoreDataFrameUnitTest.php
+++ tests/DataFrame/Core/CoreDataFrameUnitTest.php
@@ -10,14 +10,14 @@ class CoreDataFrameUnitTest extends \PHPUnit_Framework_TestCase
      */
     private $df;
 
+    private $input = [
+        ['a' => 1, 'b' => 2, 'c' => 3],
+        ['a' => 4, 'b' => 5, 'c' => 6],
+        ['a' => 7, 'b' => 8, 'c' => 9],
+    ];
+
     public function setUp()
     {
-        $this->input = [
-            ['a' => 1, 'b' => 2, 'c' => 3],
-            ['a' => 4, 'b' => 5, 'c' => 6],
-            ['a' => 7, 'b' => 8, 'c' => 9],
-        ];
-
         $this->df = DataFrame::fromArray($this->input);
     }
 
